d81a3a583b9604acba38916eaccd829e190f1bf0
Craig Wayne <craigiswayne@gmail.com>
2024-12-04 14:00:36 +0000

    Localization Dotnet Project in separate folder

--- examples/localization-dotnet/LocalizationToggler.cs
+++ examples/localization-dotnet/LocalizationToggler.cs
@@ -7,12 +7,11 @@ public partial class LocalizationToggler : Button
 	private int _languageIndex = 1;
 	private readonly List<string> _languages = new() {"en", "es"};
 	
-	public new void ButtonUp() {
+	public new void ButtonUp()
+	{
 		ChangeLanguage(_languages[_languageIndex]);
 		_languageIndex = (_languageIndex + 1) % _languages.Count;
 	}
-	
-	
 	
 	private void ChangeLanguage(string languageIso)
 	{
