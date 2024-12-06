7baa091aa4b08c4f73ef8fe1dcd1a7451186a0e9
Craig Wayne <craigiswayne@gmail.com>
2024-12-06 09:54:53 +0000

    Yay working, calling rest function then injecting code

--- examples/localization/localization-api-dotnet/LocalizationInjector.cs
+++ examples/localization/localization-api-dotnet/LocalizationInjector.cs
@@ -1,23 +1,40 @@
 using Godot;
 using System.Linq;
+using System.Text;
 using Godot.Collections;
 
+/// <summary>
+/// <para>Fetches Latvian translations from a server and injects it into the game</para>
+/// <para>Docs: https://docs.godotengine.org/en/stable/tutorials/networking/http_request_class.html</para>
+/// </summary>
 public partial class LocalizationInjector : Button
 {
+	private const string LanguageIso = "lv";
+
 	private new void ButtonUp()
 	{
-		const string languageIso = "lv";
 
-		if (TranslationServer.GetLoadedLocales().Contains(languageIso))
+		if (TranslationServer.GetLoadedLocales().Contains(LanguageIso))
 		{
-			GD.Print($"Locale: {languageIso} already loaded");
+			GD.Print($"Locale: {LanguageIso} already loaded");
 			return;
 		}
+
+		var httpRequest = GetParent().GetNode<HttpRequest>("HTTPRequest");
+		httpRequest.RequestCompleted += OnRequestCompleted;
+		// TODO replace this with the endpoint that will return translations for your desired language iso
+		httpRequest.Request("https://api.github.com/repos/godotengine/godot/releases/latest");
+	}
+	
+	private static void OnRequestCompleted(long result, long responseCode, string[] headers, byte[] body)
+	{
+		// var json = Json.ParseString(Encoding.UTF8.GetString(body)).AsGodotDictionary();
+		// GD.Print(json["name"]);
 		
-		var translation = new Translation { Locale = languageIso };
+		var translation = new Translation { Locale = LanguageIso };
 		var dict = new Dictionary<string, string>
 		{
-			{"BALANCE", "SOME LATVIAN SHIT"}
+			{"BALANCE", "Something in Latvian"}
 		};
 		
 		foreach (var keyValuePair in dict)
@@ -25,6 +42,6 @@ public partial class LocalizationInjector : Button
 			translation.AddMessage(keyValuePair.Key, keyValuePair.Value);
 		}
 		TranslationServer.AddTranslation(translation);
-		TranslationServer.SetLocale(languageIso);
+		TranslationServer.SetLocale(LanguageIso);
 	}
 }
